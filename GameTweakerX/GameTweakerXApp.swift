3db71833e66ba030abb78e72b2f33950f087b785
Radim Vesely <bright4042x@yahoo.com>
2025-03-28 18:49:27 +0100

    Initial Commit

---
+++ GameTweakerX/GameTweakerXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTweakerXApp.swift
+//  GameTweakerX
+//
+//  Created by Radim Veselý on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct GameTweakerXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameTweakerXTests/GameTweakerXTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameTweakerXTests.swift
+//  GameTweakerXTests
+//
+//  Created by Radim Veselý on 28.03.2025.
+//
+
+import Testing
+@testable import GameTweakerX
+
+struct GameTweakerXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
